b4dfcfc4f2b2e1ff6a493dac7a8e326b768a5c51
kewang <cpckewang@gmail.com>
2015-07-07 23:27:06 +0800

    Add jedis (redis client for java) support and testing

--- src/main/java/hedis/io/api/DataApi.java
+++ src/main/java/hedis/io/api/DataApi.java
@@ -7,11 +7,21 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
+import redis.clients.jedis.Jedis;
+
 @Path("data")
 public class DataApi {
 	@Produces("application/json")
 	@GET
 	public Response get(@Context HttpHeaders headers, String body) {
+		Jedis jedis = new Jedis("localhost");
+
+		String value = jedis.get(headers.getHeaderString("hedis"));
+
+		System.out.println("value: " + value);
+
+		jedis.close();
+
 		return Response.ok().build();
 	}
 }
