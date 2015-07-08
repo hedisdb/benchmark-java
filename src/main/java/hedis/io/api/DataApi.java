1cfd7e7ce6dba9c1919fc2b7f193d0a679de74f1
kewang <cpckewang@gmail.com>
2015-07-08 23:16:30 +0800

    change path & body

--- src/main/java/hedis/io/api/DataApi.java
+++ src/main/java/hedis/io/api/DataApi.java
@@ -1,6 +1,6 @@
 package hedis.io.api;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
@@ -9,19 +9,19 @@ import javax.ws.rs.core.Response;
 
 import redis.clients.jedis.Jedis;
 
-@Path("data")
+@Path("hedis")
 public class DataApi {
 	@Produces("application/json")
-	@GET
+	@POST
 	public Response get(@Context HttpHeaders headers, String body) {
 		Jedis jedis = new Jedis("localhost");
 
-		String value = jedis.get(headers.getHeaderString("hedis"));
+		String value = jedis.get(body);
 
 		System.out.println("value: " + value);
 
 		jedis.close();
 
-		return Response.ok().build();
+		return Response.ok(value).build();
 	}
 }
